1257b954a16956e17760b480bf1ca27565f39185
reyarmenta <reyarmenta1@hotmail.com>
2023-04-26 23:23:31 -0600

    Creating a list and adding the numbers in it

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number = -1;
+        
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            
+        }
+
+        int sum = numbers.AsQueryable().Sum();
+        Console.WriteLine("The sum is: {0}", sum);
+    
+        double avg = Queryable.Average(numbers.AsQueryable());
+        Console.WriteLine("The average is: " + avg);
+
+        Console.WriteLine("The larges number is: " + numbers.Max());
+
     }
 }
