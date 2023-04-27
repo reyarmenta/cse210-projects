7bd7597076ad665da460ce98bb7087a73b38d917
reyarmenta <reyarmenta1@hotmail.com>
2023-04-26 22:22:30 -0600

    Random number Generator

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //This was to assign the magic number
+        //Console.Write("What is the magic number? ");
+        //string magic = Console.ReadLine();
+
+        //int num = int.Parse(magic);
+
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1,101);
+
+        int guess = -1;
+
+
+        while (guess != num)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        
+            
+            if (guess > num)
+            {
+                Console.WriteLine("Lower");                
+            }
+        
+            else if (guess < num)
+            {
+                Console.WriteLine("Higher");                
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+    
+
     }
 }
