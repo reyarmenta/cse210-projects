bbecaedf92e8aa8a3e8efa35c411af4568da1be2
reyarmenta <reyarmenta1@hotmail.com>
2023-05-06 18:39:48 -0600

    Coding Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        //This code was to print just the company
+        //Console.WriteLine($"{job1._company}");
+        //Console.WriteLine($"{job2._company}");
+
+        //Display both jobs
+        //job1.Display();
+        //job2.Display();
+
+        Resume myResume = new Resume();
+        
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //verifying to see if I can display first job Title
+        //Console.WriteLine($"{myResume._jobs[0]._jobTitle}");
+
+        myResume.Display();
+        
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Resume
+{
+    public string _name = "Reynaldo Armenta";
+    public List<Job> _jobs = new List<Job>();
+
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Jobs: ");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
